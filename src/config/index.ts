e869e0b4f147eb3dd58386f13548801c67823d6b
nvminh155 <minhnv155@gmail.com>
2025-11-27 21:40:13 +0700

    fix: run error on vercel with env

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 const envFound = dotenv.config();
-if (envFound.error) {
+if (envFound.error && process.env.VERCEL !== "1") {
   throw new Error("Couldn't find .env file");
 }
 

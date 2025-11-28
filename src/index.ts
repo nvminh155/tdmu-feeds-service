5a6d849148c09acf6656ac29cc8957fdc503db04
nvminh155 <minhnv155@gmail.com>
2025-11-28 16:25:23 +0700

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,8 @@ const corsOptions = {
   origin: [
     'http://localhost:3000', // React/Next.js local
     'https://yourdomain.com', // domain production
-    'http://192.168.43.5:3000'
+    'http://192.168.43.5:3000',
+    'https://feeds.tdmu.xyz'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
